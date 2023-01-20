f4466e773e3d91a8bf9b032eead08f5a37542ece
jiangmuran <wd.jiang@foxmail.com>
2023-01-20 20:10:35 +0800

    tiv

--- main.py
+++ main.py
@@ -1,54 +1,6 @@
 # -*- coding: UTF-8 -*-
 
-'''
-# Little_Wolf
 
-**A Werewolf Bot For HackChat**  
-为hack.chat量身打造的狼人杀游戏机器人
-
-**Author:paperee(ee) jiangmuran(jmr)**  
-ee：我觉得class实在没必要 所以没用  
-请随意吐槽我臃肿的屎山代码  
-不能对我的uwu变量名有意见(被打)
-
-**Thanks:Maggie zzChumo xuan2wei1 4n0n4me jmr DPG IAmLonelyWhoCanSaveMe 23**  
-感谢以上帮忙测试的小伙伴们
-
-## 【使用方法】
-**更改【设置区域】**  
-**安装库**：pip install websocket-client  
-**运行(以实际情况为准)**：python main.py
-
-## 【更新记录】
-### uwu.1.00 2023.01.18
-- 完成了机器人框架
-
-### uwu.1.08 2023.01.19**
-- 修复了老版本的BUG(详见BUG报告)
-- 优化了储存数据库结构
-- 优化了代码结构(还是屎山)
-- 修改了部分变量名(强迫症)
-- 修改了女巫的阵营
-- 添加了新的操作和变更项
-- 添加了遗言功能
-- 添加了特殊项自动强调的功能
-- 添加了自动判断消息类型的功能
-
-## 【是否解决｜已知BUG报告｜修复人员】
-- x｜有时狼人的场合重复出现
-- x｜已知身份后还会显示请睁眼
-- v｜开始后无法停止/让村长下线｜jiangmuran(jmr)
-- v｜变更牌名后部分无效｜paperee(ee)
-- v｜变更牌组后无法自动开始｜paperee(ee)
-- v｜结束后胜利方存活为0｜paperee(ee)
-- v｜结束后女巫的用药不显示｜paperee(ee)
-'''
-
-
-'''
-【设置区域】
-改加入地址&频道 bot昵称 bot识别码
-'''
 
 # 地址列表:hack.chat
 hc="wss://hack.chat/chat-ws"
@@ -91,7 +43,7 @@ import threading
 try:
 	# 主要数据库
 	data={"uwu":False}
-
+	ver="uwu1.10"
 	# 卡牌列表
 	card={0:{True:"好人",False:"坏人"},
 	1:["平民","夜晚：注意旁白 安静等待\n白天：提出意见 跟随大家投票",True],
@@ -214,7 +166,7 @@ try:
 					send(roll(emote))
 
 				elif text=="|help":
-					send(f"·\n【{other['game']}】\n开发者：paperee(ee),jiangmuran(jmr)\n机器人：{botnick}\n版本号：uwu.1.08\n·\n使用手册\n查看帮助：|help\n查看规则：|rules\n获取权限：|root\n开始游戏：|start")
+					send(f"·\n【{other['game']}】\n开发者：paperee(ee),jiangmuran(jmr)\n机器人：{botnick}\n版本号：{ver}\n·\n使用手册\n查看帮助：|help\n查看规则：|rules\n获取权限：|root\n开始游戏：|start")
 
 				elif text=="|rules":
 					note=str()
@@ -437,7 +389,7 @@ try:
 													dead=[_ for _ in data["state"][False] if _ not in data["sudden"]]
 													alive=[_ for _ in data["state"][True] if _!=uwu[0]]
 
-													uwu_=" ".join([f"[{data['witch']['med'][_][1]}]" for _ in med])
+													uwu_=" ".join([f"[{data['witch']['med'][_][0]}]" for _ in med])
 													_uwu=str()
 
 													if dead and True in med:
