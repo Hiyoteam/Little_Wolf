152bedf65bf5529a0095387a259cf9479ce752aa
ee <97789520+paperee@users.noreply.github.com>
2023-01-24 00:44:26 +0800

    uwu.1.19｜2023.01.23｜paperee(ee)
    uwu

--- updatewolf.sh
+++ updatewolf.sh
@@ -1,6 +1,6 @@
 sudo rm -rf /Little_wolf
 cd /
-sudo git clone https://www.github.com/hiyoteam/Little_wolf
-cd Little_wolf
+sudo git clone https://www.github.com/Hiyoteam/Little_wolf
+cd /Little_wolf
 sudo chmod 777 *
 sudo nohup python3 main.py &
