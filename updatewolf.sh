059ba19c6125fa49ccc01b612fb310f72d1d9354
huolongguo1O <121071167+huolongguo1O@users.noreply.github.com>
2023-02-09 15:17:19 +0800

    Update updatewolf.sh

--- updatewolf.sh
+++ updatewolf.sh
@@ -1,6 +1,27 @@
-sudo rm -rf /Little_wolf
-cd /
-sudo git clone https://www.github.com/Hiyoteam/Little_wolf
-cd /Little_wolf
-sudo chmod 777 *
-sudo nohup python3 main.py &
+install(){
+    echo Installing
+    git clone https://www.github.com/Hiyoteam/Little_Wolf
+    ./Little_wolf
+    chmod 777 *
+    nohup python3 main.py &
+}
+update(){
+    echo Updating
+    cp config.py /tmp/config.py
+    cd ..
+    rm -rf ./Little_Wolf
+    git clone https://www.github.com/Hiyoteam/Little_Wolf
+    cd ./Little_Wolf
+    cp /tmp/config.py config.py
+    chmod 777 *
+    nohup python3 main.py &
+}
+testifinstall=$(pwd|grep Little_Wolf)
+if [[ "$testifinstall" != "" ]]
+then 
+    echo Installed
+    update
+else
+    echo NotFound
+    install
+fi
